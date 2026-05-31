5bd42bdfbfd117dbce65e1a2bef3813d63536b4f
Hamza Abdulhakim <abdulhakimhamza919@gmail.com>
2026-05-31 20:33:52 +0300

    Personal website

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// const menuBtn = document.getElementById('menu-btn');
-// const sidebar = document.getElementById('sidebar');
-// 
-// menuBtn.addEventListener('click', () => {
-    // sidebar.classList.toggle('active');
-// });
+const menuBtn = document.getElementById('menu-btn');
+const sidebar = document.getElementById('sidebar');
 
-var adult = prompt("Are you greater than 18?");
-if(adult)
-    alert("yes");
-else
-    alert("No!");
+menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
